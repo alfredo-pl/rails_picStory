81222dbd71ab810c3aa8f2a5a0f247db7341a510
alfredopl <aalfredoplz@gmail.com>
2021-02-02 11:50:00 -0300

    models

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,9 @@
 class User < ApplicationRecord
     has_secure_password
     validates :email, presence: true, uniqueness: {case_sensitive: true}
+    has_many :stories , dependent: :destroy
+
+    def get_stories
+        Story.references(:users).where(user_id: id)
+    end
 end
